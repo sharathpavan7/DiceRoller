dc7e9981ebe411b844fa67d702940c80d7b2a6a6
Sharath <sharathpavan7@gmail.com>
2020-03-12 22:16:23 +0530

    commit - v0.0.1 showing random numbers in TextView on button click.

--- app/src/main/java/com/sharath/diceroller/MainActivity.kt
+++ app/src/main/java/com/sharath/diceroller/MainActivity.kt
@@ -2,11 +2,33 @@ package com.sharath.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+//import com.sharath.diceroller.databinding.ActivityMainBinding
+import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
 
+    //private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        /*binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)*/
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+
+        //binding.rollButton.text = "Lets roll"
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        val result = Random.nextInt(6)+1
+        resultText.text = result.toString()
     }
 }
