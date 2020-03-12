cce0f8b8d9eab7423854f77c7bd7de388f1bca64
Sharath <sharathpavan7@gmail.com>
2020-03-12 23:36:04 +0530

    commit - v0.0.1 code optimization. global variable for Dice image & initializing in onCreate method insted in rollDice method.

--- app/src/main/java/com/sharath/diceroller/MainActivity.kt
+++ app/src/main/java/com/sharath/diceroller/MainActivity.kt
@@ -9,7 +9,7 @@ import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var diceImage: ImageView
     //private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +23,7 @@ class MainActivity : AppCompatActivity() {
             rollDice()
         }
 
+        diceImage = findViewById(R.id.dice_image)
         //binding.rollButton.text = "Lets roll"
     }
 
@@ -36,8 +37,6 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-
-        val diceImage: ImageView = findViewById(R.id.dice_image)
         diceImage.setImageResource(drawableResource)
     }
 }
